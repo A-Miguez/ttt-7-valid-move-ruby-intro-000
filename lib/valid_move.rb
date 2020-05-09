6f28ff3ad65fab730eee3bdee22b4d4244ca33ec
Anthony Miguez <anm501@stern.nyu.edu>
2020-05-09 23:58:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
   if index.between?(0,8) && position_taken?
-    true
+    return true
   else
-    false
+    return false
   end
 end
 
